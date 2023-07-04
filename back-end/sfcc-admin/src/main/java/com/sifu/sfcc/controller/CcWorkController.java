cc80c65e0a0d1937001cf37beff00865604bf892
Lipu_jun <wzp0324@sina.com>
2023-07-04 21:30:04 +0800

    feat：完成了工单查询的后端部分 todo：需要测试

--- back-end/sfcc-admin/src/main/java/com/sifu/sfcc/controller/CcWorkController.java
+++ back-end/sfcc-admin/src/main/java/com/sifu/sfcc/controller/CcWorkController.java
@@ -1,16 +1,16 @@
 package com.sifu.sfcc.controller;
 
+import com.github.pagehelper.PageHelper;
 import com.sifu.sfcc.common.api.CommonResult;
 import com.sifu.sfcc.dto.CcWorkOrderParam;
 import com.sifu.sfcc.model.CcWorkOrder;
 import com.sifu.sfcc.service.CcWorkOrderService;
+import com.sifu.sfcc.vo.workorder.WorkOrderVO;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/wonder")
@@ -23,5 +23,18 @@ public class CcWorkController {
         CcWorkOrder ccWorkOrder = ccWorkOrderService.create(ccWorkOrderParam);
         return CommonResult.success(1);
     }
-
+    public CommonResult list(
+            @RequestParam(required = false) Integer pageSize,
+            @RequestParam(required = false) Integer pageNum,
+            @RequestParam(required = false) Integer wstatus,
+            @RequestParam(required = false) String toDoText) {
+        PageHelper.startPage(pageNum,pageSize);
+        List<CcWorkOrder> list = ccWorkOrderService.list(pageSize, pageNum, wstatus, toDoText);
+        return CommonResult.success(list);
+    }
+    @GetMapping("/getByNo/{code}")
+    public CommonResult getByNo(String code,@RequestParam String worderCode){
+        WorkOrderVO workOrderVO = ccWorkOrderService.qryByOrderNo(worderCode);
+        return CommonResult.success(1);
+    }
 }
